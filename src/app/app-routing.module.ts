91d550b7265a1427c59770bccfcc12a6ac81e59f
Sasa Zikic <sasazikic2@gmail.com>
2024-12-05 20:20:04 +0100

    initialization of dashboard and form component and setting up a router for them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { FormComponent } from './modules/form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "form", component: FormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { FormComponent } from './modules/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
